e7cca1ac9259c1a891e606f87059925855ec03fc
farouk527 <79808552+farouk527@users.noreply.github.com>
2023-01-03 17:05:57 +0100

    Create user.js

---
+++ model/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type : String , unique : true ,required : true
+    } , 
+    password: {type : String , required : true
+    } , 
+    token : {type : String}
+
+});
+module.exports = mongoose.model("user",userSchema);
